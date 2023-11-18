150666fa13effe444fd37fa2a2c502b9184ac529
njiking <kingsleynji5@gmail.com>
2023-11-18 09:52:38 +0100

    main

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,4 +2,42 @@ document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
   });
 
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('nav-links');
+  
+  hamburger.addEventListener('click', () => {
+          hamburger.classList.toggle('active');
+          nav.classList.toggle('active');
+  });
+
+  // Initialize the phone number mask
+var phoneMask = IMask(document.getElementById("phone"), {
+  mask: "(000) 000-0000"
+});
+
+// Handle form submission
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        alert("Form submitted!");
+      } else {
+        console.log(response);
+        alert("Something went Wrong!");
+      }
+    });
+  });
+
+
     
