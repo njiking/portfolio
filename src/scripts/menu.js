a888d2e9038946507555a39b59d7a367af971a46
njiking <kingsleynji5@gmail.com>
2023-12-15 18:56:45 +0100

    main

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,65 +1,72 @@
 // close nav in mobile
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navLinks.classList.toggle('expanded');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("expanded");
 });
 
-const links = document.querySelectorAll('.nav-links a');
+const links = document.querySelectorAll(".nav-links a");
 
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navLinks.classList.remove('expanded');
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("expanded");
   });
 });
-
-
 
 // Handle form submission
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: json
-    }).then(async (response) => {
-      let json = await response.json();
-      if (response.status == 200) {
-        alert("Form submitted!");
-      } else {
-        console.log(response);
-        alert("Something went Wrong!");
-      }
-    });
+  // Assuming you have a formData object containing your form data
+  let formData = new FormData(document.getElementById("contactForm"));
+  
+  // Convert formData to JSON
+  let jsonData = {};
+  formData.forEach((value, key) => {
+    jsonData[key] = value;
   });
+  let json = JSON.stringify(jsonData);
 
-  // quick upscroll
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: json,
+  }).then(async (response) => {
+    if (response.status == 200) {
+      alert("Form submitted!");
+    } else {
+      console.log(response);
+      alert("Something went wrong!");
+    }
+  });
+});
+
+
+// quick up scroll
 if (window.innerWidth > 699) {
-document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("DOMContentLoaded", function () {
     var scrollUpBtn = document.getElementById("scrollUpBtn");
     window.addEventListener("scroll", function () {
-        scrollUpBtn.style.display = (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? "block" : "none";
+      scrollUpBtn.style.display =
+        document.body.scrollTop > 20 || document.documentElement.scrollTop > 20
+          ? "block"
+          : "none";
     });
     scrollUpBtn.addEventListener("click", function () {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+      window.scrollTo({ top: 0, behavior: "smooth" });
     });
-});
+  });
 }
 
-  // loading screen
-document.addEventListener("DOMContentLoaded", function() {
-  setTimeout(function() {
-    document.querySelector('.loading-screen').style.display = 'none';
+// loading screen
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.querySelector(".loading-screen").style.display = "none";
   }, 1000);
 });
-
-
